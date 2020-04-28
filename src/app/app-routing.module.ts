e925f022ebde0ff98f5eb41ca82aef2bd341e110
tucsonSun <x_ernie_x@hotmail.com>
2020-04-28 06:42:32 -0700

    moving folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {ProfileComponent} from "./pages/profile/profile.component";
 import {PortfolioComponent} from "./pages/portfolio/portfolio.component";
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  //{ path: 'login', component: LoginComponent },
-  //{ path: 'register', component: RegisterComponent },
+    {path: 'home', component: HomeComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'portfolio', component: PortfolioComponent},
+    // { path: 'login', component: LoginComponent },
+    // { path: 'register', component: RegisterComponent },
 
-  // otherwise redirect to home
-  { path: '', redirectTo: 'home ', pathMatch: 'full'},
-  { path: '**', redirectTo: 'home' }
+    // otherwise redirect to home
+    {path: '', redirectTo: 'home ', pathMatch: 'full'},
+    {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
-import { TopMenuComponent } from './top-menu/top-menu.component';
-import {AngularMaterialModule} from "./angular-material.module";
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { AngularMaterialModule } from "./modules/angular-material.module";
 import { ServicesComponent } from './components/services/services.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
