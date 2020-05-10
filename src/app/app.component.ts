13dcd2b8209615ccf668ea7186e39b673c7fc119
tucsonSun <x_ernie_x@hotmail.com>
2020-05-10 14:51:24 -0700

    added styling to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hawkins-architecture';
+  public title = 'hawkins-architecture';
 }
