bdf7e7ff44ebd05fe74166f2921da483a1d87899
tucsonSun <x_ernie_x@hotmail.com>
2020-05-10 17:02:21 -0700

    added styling to app

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,19 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  public appStyleName = 'my-light-theme';
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public toggleStyle(): void {
-    if (this.appStyleName === 'my-light-theme') {
-      this.appStyleName = 'my-dark-theme';
-    } else {
-      this.appStyleName = 'my-light-theme';
-    }
-  }
 
 }
