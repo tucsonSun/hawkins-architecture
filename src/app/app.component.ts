ec1d41384abfc971c2a19b44486504add09f6029
tucsonSun <x_ernie_x@hotmail.com>
2020-05-10 17:02:00 -0700

    added styling to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   public title = 'hawkins-architecture';
+  public appMyThemeStyleName = 'my-light-theme';
+
+  constructor(private renderer: Renderer2,  private el: ElementRef) { }
+
+
+  ngOnInit(): void {
+    this.addClass();
+  }
+
+  public toggleStyle(): void {
+    this.removeClass(); //removed old class
+
+    if (this.appMyThemeStyleName === 'my-light-theme') {
+      this.appMyThemeStyleName = 'my-dark-theme';
+    } else {
+      this.appMyThemeStyleName = 'my-light-theme';
+    }
+
+    this.addClass(); //add new class
+  }
+
+  private addClass(): void {
+    this.renderer.addClass(this.el.nativeElement, this.appMyThemeStyleName);
+  }
+
+  private removeClass(): void {
+    this.renderer.removeClass(this.el.nativeElement, this.appMyThemeStyleName);
+  }
 }
