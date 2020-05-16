8248c1705d18673a61d3ed06d933c081167b0712
tucsonSun <x_ernie_x@hotmail.com>
2020-05-16 16:00:47 -0700

    progress on profile sub menus

--- src/app/pages/portfolio/portfolio-routing.module.ts
+++ src/app/pages/portfolio/portfolio-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {PortfolioComponent} from "./portfolio.component";
 import {PortfolioSpecialtySchoolsComponent} from "./portfolio-specialty-schools/portfolio-specialty-schools.component";
 import {PortfolioServiceFacilitiesComponent} from "./portfolio-service-facilities/portfolio-service-facilities.component";
+import {PortfolioTechnicalEducationComponent} from "./portfolio-technical-education/portfolio-technical-education.component";
+import {PortfolioCommunityComponent} from "./portfolio-community/portfolio-community.component";
 
 
-export const paths = {
+export const PORFILE_PATHS = {
   PORTFOLIO: 'portfolio',
   CHILD_PORTFOLIO_SPECIALITY: 'specialty-schools',
+  CHILD_PORTFOLIO_TECH_EDUCATION: 'technical-education',
   CHILD_PORTFOLIO_FACILITIES: 'service-facilities',
+  CHILD_PORTFOLIO_COMMUNITY: 'cultural-community',
 }
 
 
@@ -19,13 +23,23 @@ const routes: Routes = [
   },
     // Child component
   {
-    path: paths.CHILD_PORTFOLIO_SPECIALITY,
+    path: PORFILE_PATHS.CHILD_PORTFOLIO_SPECIALITY,
     component: PortfolioSpecialtySchoolsComponent
   },
   // Child component
   {
-    path: paths.CHILD_PORTFOLIO_FACILITIES,
+    path: PORFILE_PATHS.CHILD_PORTFOLIO_TECH_EDUCATION,
+    component: PortfolioTechnicalEducationComponent
+  },
+  // Child component
+  {
+    path: PORFILE_PATHS.CHILD_PORTFOLIO_FACILITIES,
     component: PortfolioServiceFacilitiesComponent
+  },
+  // Child component
+  {
+    path: PORFILE_PATHS.CHILD_PORTFOLIO_COMMUNITY,
+    component: PortfolioCommunityComponent
   },
 ];
 
