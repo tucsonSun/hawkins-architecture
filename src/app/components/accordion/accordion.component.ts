c24a6c21f06bbf729f3f51e5b0d1860a60c7f0e6
tucsonSun <x_ernie_x@hotmail.com>
2020-10-06 07:24:42 -0700

    added accordion starter component

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.scss']
 })
 export class AccordionComponent implements OnInit {
+
+  @Input() title = 'NA';
 
   constructor() { }
 
